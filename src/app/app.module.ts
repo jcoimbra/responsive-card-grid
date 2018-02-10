8c1560fe936f3c46d46f9de7081bcf2591d1ca62
José Coimbra <jose.coimbra@coriant.com>
2018-02-10 09:04:47 +0000

    Initial grid commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
